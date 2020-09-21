91c76088299d3933d6a89a08c23e7ecc9e8aa75d
Edan Schwartz <edanschwartz@gmail.com>
2020-09-21 09:41:25 -0500

    Add images

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -4,7 +4,31 @@ const galleryItems = [
         path: 'images/goat_small.jpg', 
         description: 'Photo of a goat taken at Glacier National Park.', 
         likes: 0 
-    }
+    },
+    { 
+        id: 1, 
+        path: 'images/dooper.png', 
+        description: 'Dooper Coten (attempt 2)', 
+        likes: 5 
+    },
+    { 
+        id: 1, 
+        path: 'images/emma.png', 
+        description: 'Emma striking a pose.', 
+        likes: 3
+    },
+    { 
+        id: 1, 
+        path: 'images/jake.png', 
+        description: 'Jake looking fresh', 
+        likes: 1
+    },
+    { 
+        id: 1, 
+        path: 'images/tyler.png', 
+        description: 'Tyler feeling monday', 
+        likes: 175
+    },
 ];
 
 module.exports = galleryItems;
