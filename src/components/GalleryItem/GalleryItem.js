9dbea0643a67a6ab626a917187e0da9183ec87e0
Edan Schwartz <edanschwartz@gmail.com>
2020-09-21 10:27:10 -0500

    Conditional rendering for gallery item

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
 
 class GalleryItem extends Component {
+
+  state = {
+    showDescription: false
+  };
+
+
+  toggleDescription = () => {
+    this.setState({
+      showDescription: !this.state.showDescription
+    });
+  }
+
   render() {
     return (
       <div>
-        <img
-           src={this.props.item.path} 
-           alt={this.props.item.description} 
-         />
+        {/* Image or Description */}
+        <div onClick={this.toggleDescription}>
+          {
+            this.state.showDescription ?
+
+              <span>{this.props.item.description}</span> :
+
+              <img
+                src={this.props.item.path} 
+                alt={this.props.item.description} 
+              />
+          }
+        </div>
+
+        {/* Like Button */}
+        <div>
+          <button>Love it!</button>
+        </div>
+
+        {/* Display current likes */}
+        <div>
+          {
+            this.props.item.likes === 0 ?
+              `Nobody likes this :(` :
+              `${this.props.item.likes} people love this!`
+          }
+        </div>
       </div>
     );
   }
